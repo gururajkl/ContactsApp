f444d4d4cf577ee4abc586c638580d562ed21cf1
gururaj <gururajragavendra7@gmail.com>
2022-12-08 22:36:02 +0530

    Add project files.

---
+++ ContactsApp/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using SQLite;
+using ContactsApp.Classes;
+
+namespace ContactsApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ReadDataBase();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NewContactWindow newContactWindow = new NewContactWindow();
+            newContactWindow.ShowDialog();
+            ReadDataBase();
+        }
+
+        void ReadDataBase()
+        {
+            List<Contact> contacts;
+
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<Contact>();
+                // Similar to Select SQL stmt 
+                contacts = connection.Table<Contact>().ToList();
+            }
+
+            if(contacts != null)
+            {
+                contactsLV.ItemsSource = contacts;
+            }
+        }
+    }
+}
---
+++ ContactsApp/NewContactWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using ContactsApp.Classes;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ContactsApp
+{
+    /// <summary>
+    /// Interaction logic for NewContactWindow.xaml
+    /// </summary>
+    public partial class NewContactWindow : Window
+    {
+        public NewContactWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Getting value from UI and setting those to contact object
+            Contact contact = new Contact()
+            {
+                Name = nameTextBox.Text,
+                Email = emailTextBox.Text,
+                Phone = phoneTextBox.Text,
+            };
+
+            // Database operations
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                // Creating table of Contact type with properties defined
+                connection.CreateTable<Contact>();
+
+                // Inserting object (has UI values)
+                connection.Insert(contact);
+
+                // No use of conn.Close() bcz SQLite implements IDisposable which gonna close once it comes out of using
+            }
+
+            Close();
+        }
+    }
+}
